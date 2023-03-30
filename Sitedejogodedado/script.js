e86b3428093f46a9a1d65783c8f0eaf4b03aec9e
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2023-03-30 10:06:41 -0300

    Update script.js

--- Sitedejogodedado/script.js
+++ Sitedejogodedado/script.js
@@ -2,7 +2,13 @@
 const resultado = document.getElementById("resultado");
 const botao = document.getElementById("botao");
 
-botao.addEventListener("click",function(){
-    const numeroAleatorio = Math.floor(Math.random()* 6) + 1;
-    resultado.innerHTML = numeroAleatorio;
-});
+// botao.addEventListener("click",function(){
+   // const numeroAleatorio = Math.floor(Math.random()* 6) + 1;
+    //resultado.innerHTML = numeroAleatorio;
+//});
+const calcular = () =>{
+    const numeroAleatorio = Math.floor(Math.random() *6) +1;
+    resultado.innerHTML = numeroAleatorio; 
+}
+
+botao.onclick = calcular;
